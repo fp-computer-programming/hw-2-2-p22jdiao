ceac212e7becf497b692700e73af588d6587818b
p22jdiao <p22jdiao@fairfieldprep.org>
2021-09-20 10:20:41 -0400

    first version

---
+++ hw2-2.py
@@ -0,0 +1,13 @@
+# Author: JD 09/17/2021
+
+# ft = int(input("number of free throw:"))
+
+# twopts = int(input("number of 2 points:"))
+
+# threepts = int(input("number of 3 points:"))
+
+
+# score = ft + 2 * (twopts) + 3 * (threepts)
+
+# print("The player scored", score, "points in the game.")
+
